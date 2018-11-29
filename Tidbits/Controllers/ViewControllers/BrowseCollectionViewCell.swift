e56635d436529aeb90782feff8936175c3bfb916
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-11-29 15:00:13 -0700

    Created custom collection view, made files for the collection view and the cell and dragged out respective outlets

---
+++ Tidbits/Controllers/ViewControllers/BrowseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseCollectionViewCell.swift
+//  Tidbits
+//
+//  Created by Xavier on 11/29/18.
+//  Copyright © 2018 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+class BrowseCollectionViewCell: UICollectionViewCell {
+    
+}
